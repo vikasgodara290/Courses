e4459a0b092c1569c47462e688797e455bc76e83
vikas godara <vikasgodara00@outlook.com>
2024-04-21 16:35:16 +0530

    Updated on 21-Apr-2024 at 16:35

---
+++ Courses/Controllers/FilesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Courses.Controllers
+{
+    public class FilesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Files()
+        {   
+            return View();
+        }
+    }
+}
